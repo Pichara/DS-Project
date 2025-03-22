bdbb68c5bded198291f9617c34578adaacc2b70c
tattianacruz <lcruzdiaz7164@conestogac.on.ca>
2025-03-22 17:03:22 -0400

    Update tatianaStuff.h

--- DataStructureFinal/tatianaStuff.h
+++ DataStructureFinal/tatianaStuff.h
@@ -1,6 +1,50 @@
 //Avoiding multiple inclusions
+//Undo = Stack part
 #pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+
+
+//importing nickStuff.h to use for ActionType and Action structures
+#include "nickStuff.h"
+
+typedef enum {
+    ADD_ACTION = 1,
+    REMOVE_ACTION,
+    UPDATE_ACTION,
+    UNKNOWN_ACTION  
+} ActionType;
+
+typedef struct {
+    ActionType actionType;
+    char description[100];
+    char details[100];
+} Action;
+
+typedef struct PileNode {
+    Action action;
+    struct PileNode* next;
+} PileNode;
+
+typedef struct {
+    PileNode* top;
+} Pile;
+
+extern Pile actionHistory;
+
+
+// Initialize the stack ✅
+// Check if the stack is empty ✅
+// Add an action to the stack ✅
+// Pop the most recent action from the stack ✅
+// Undo the last action (using the stack) ...
+
+void initStack(Pile* stack);
+int isEmpty(Pile* stack);
+void push(Pile* stack, Action action);
+Action pop(Pile* stack);
+void undo_last_action(Pile* actionHistory);
+void clearStack(Pile* stack);
