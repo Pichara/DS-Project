513332041afba271e0c416367f4adfc49623c6ec
tattianacruz <lcruzdiaz7164@conestogac.on.ca>
2025-03-26 00:41:25 -0400

    Update tatianaStuff.h

--- DataStructureFinal/tatianaStuff.h
+++ DataStructureFinal/tatianaStuff.h
@@ -1,16 +1,12 @@
 //Avoiding multiple inclusions
-//Undo = Stack part
 #pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//Importing nickStuff.h to use for ActionType and Action structures
-#include "nickStuff.h"
-
-typedef enum ActionType {
-    ADD_ACTION = 1,
+typedef enum {
+    ADD_ACTION,
     REMOVE_ACTION,
     UPDATE_ACTION,
     UNDO_ACTION,
@@ -18,25 +14,43 @@ typedef enum ActionType {
     UNKNOWN_ACTION
 } ActionType;
 
+//Structure to store an action
 typedef struct Action {
     int actionType;
     char description[100];
     char details[100];
 } Action;
 
+//Node structure for the action stack (pile)
 typedef struct PileNode {
     Action action;
     struct PileNode* next;
 } PileNode;
 
+//Structure for the stack (pile) to store actions
+
 typedef struct Pile {
     PileNode* top;
 } Pile;
 
+//A struct for showing undo options (can store actions in a more structured way if needed)
+
+typedef struct ShowUndoOptions {
+    Action* actions;
+    int size;
+    int capacity;
+} Undo;
+
+
+extern struct Pile* LastAction;
+
 Pile* initPile();
-PileNode* initPileNode();
 int isEmpty(Pile* stack);
 void push(Pile* stack, Action action);
 Action pop(Pile* stack);
 void undo_last_action(Pile* actionHistory);
-void clearStack(Pile* stack);
+void clearStack(Pile** stack);
+void recordAction(Pile* LastAction, ActionType actionType, const char* description, const char* details);
+void showUndoOptions(Pile* actionHistory);
+void actionHistory(Pile* actionHistory);
+void showActionHistory(Pile* actionHistory);
